7b641593e0faf9b131b0471df01f758e0f30d44f
valiev <worxvaliev@gmail.com>
2025-07-16 18:13:59 +0500

    Update src

--- backend/manage.py
+++ backend/manage.py
@@ -4,19 +4,19 @@ import os
 import sys
 
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.abspath(os.path.join(BASE_DIR, '..'))
+sys.path.append(PROJECT_ROOT)
+
 def main():
-    """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
         raise ImportError(
-            "Couldn't import Django. Are you sure it's installed and "
-            "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
+            "Couldn't import Django..."
         ) from exc
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
