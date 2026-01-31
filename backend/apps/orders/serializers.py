30c91e05659e39ce6133d1b8fa8e587514ee0532
Shit-coin-v <131849250+Shit-coin-v@users.noreply.github.com>
2026-01-31 13:04:11 +0900

    Move OrderDetailSerializer to orders domain

--- backend/apps/orders/serializers.py
+++ backend/apps/orders/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from apps.main.models import Product
+from apps.main.models import Order, OrderItem, Product
 
 
 class ProductListSerializer(serializers.ModelSerializer):
@@ -24,3 +24,40 @@ class ProductListSerializer(serializers.ModelSerializer):
         if obj.image:
             return obj.image.url
         return None
+
+
+class OrderItemDetailSerializer(serializers.ModelSerializer):
+    product_code = serializers.CharField(source="product.product_code", read_only=True)
+    name = serializers.CharField(source="product.name", read_only=True)
+
+    class Meta:
+        model = OrderItem
+        fields = [
+            "product_code",
+            "name",
+            "quantity",
+            "price_at_moment",
+        ]
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+    status_display = serializers.CharField(source="get_status_display", read_only=True)
+    items = OrderItemDetailSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Order
+        fields = [
+            "id",
+            "created_at",
+            "status",
+            "status_display",
+            "payment_method",
+            "fulfillment_type",
+            "address",
+            "phone",
+            "comment",
+            "products_price",
+            "delivery_price",
+            "total_price",
+            "items",
+        ]
